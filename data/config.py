921ea3b93f533284b5a277b0bdfdc4e608a0b737
astepanov2 <astepanov@emphasoft.com>
2024-07-09 15:52:08 +0300

    more config.py refactoring with env.template creating

--- data/config.py
+++ data/config.py
@@ -6,15 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-DATABASE_DIR = pathlib.Path.cwd()
 
 WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
 WEBHOOK_PATH = f'/{BOT_TOKEN}'
 WEBHOOK_URL = ''.join([WEBHOOK_HOST, WEBHOOK_PATH])
 
 WEBAPP_HOST = os.getenv('WEBAPP_HOST')
-WEBAPP_PORT = os.environ.get('PORT')
+WEBAPP_PORT = os.getenv('WEBAPP_PORT')
 
-DATABASE_ENGINE = 'sqlite:///'
-DATABASE_NAME = 'databasequiz.db'
-DATABASE = os.path.join([DATABASE_ENGINE, DATABASE_DIR, DATABASE_NAME])
+DB_DIR = pathlib.Path.cwd()
+DB_ENGINE = 'sqlite:///'
+DB_NAME = f"{os.getenv('DB_NAME', 'databasequiz')}.db"
+DB_BASE = os.path.join([DB_ENGINE, DB_DIR, DB_NAME])

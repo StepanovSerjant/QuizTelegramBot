2474938e65cdff5cab7c88fd6c5bf6dd76b72a89
astepanov2 <astepanov@emphasoft.com>
2024-07-09 16:15:24 +0300

    create_db imports fixed

--- database/utils.py
+++ database/utils.py
@@ -1,3 +1,10 @@
+from sqlalchemy_utils import database_exists
+
+from database.models import Base
+from database.services import set_new_quiz
+from database.settings import engine
+
+
 def create_db(new_quiz: bool = False) -> None:
     if not database_exists(engine.url):
         Base.metadata.create_all(engine)
